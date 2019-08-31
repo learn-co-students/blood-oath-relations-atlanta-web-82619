b94eabe85adac438276fee5f60ded44190f242e3
Andrea T <tostonar@gmail.com>
2019-08-31 07:26:58 -0400

    Finished advanced methods

---
+++ app/models/follower.rb
@@ -0,0 +1,38 @@
+class Follower
+  attr_reader :name, :age
+  attr_accessor :life_motto
+
+  @@all = []
+  def initialize(name, age, life_motto)
+    @name = name
+    @age = age
+    @life_motto = life_motto
+    @@all << self
+  end
+
+  def cults
+    BloodOath.all.select {|oath| oath.follower == self }.map {|oath| oath.cult}
+  end
+
+  def how_many_cults
+    cults.size
+  end
+
+  def my_cults_slogans
+    cults.each {|cult| puts "#{cult.slogan}"}
+  end
+
+  def join_cult(cult)
+    oath = BloodOath.new(cult, self)
+    oath.follower = self
+  end
+
+  def self.most_active
+    @@all.max {|follower1, follower2| follower1.cults.size <=> follower2.cults.size}
+  end
+
+  #DONE : top 10 most active followers, follower in most cults
+  def self.top_ten
+    @@all.max(10) {|f1, f2| f1.how_many_cults <=> f2.how_many_cults}
+  end
+end
